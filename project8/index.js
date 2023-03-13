f9ee9146bb31fb477d0eab0e9a2dbff71abbdd48
Venyaba <wenseslasbar@gmail.com>
2023-03-13 11:43:13 +0100

    hw js3

--- project8/index.js
+++ project8/index.js
@@ -1,34 +1,6 @@
+function func( a = 0){
+    console.log(a * 2)
+}
 
-// const num = prompt('Введите число:')
-// console.log(num/10)
-
-// const num1 = prompt('Введите число:')
-// const num2 = prompt('Введите число:')
-
-// if(num1 < num2){
-// console.log(num1)
-// }else{
-//     console.log(num2)
-// }
-
-
-// const num3 = prompt('Введите число:')
-// if(num3 == 100){
-//     console.log('Число равно 100')
-// }else if( num3 > 100){
-//     console.log('Число больше 100')
-// }else{
-//     console.log('Число меньше 100')
-// }
-
-
-// const name = prompt('Введите ваше имя:')
-// const age = prompt('Введите ваш возраст:')
-
-// const adult = 18
-
-// if( age > adult){
-//     console.log(`Hello, ${name}`)
-// }else{
-//     console.log(`Hi, ${name}`)
-// }
+func(2)
+func(5)
